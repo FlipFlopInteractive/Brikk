d66768c0e78451df3906810c3c87a2ccb52b97bc
neilVerhavert <neil.verhavert@gmail.com>
2015-03-22 15:19:29 +0100

    fixing stufffffffffff

--- src/wp-content/themes/twentyfifteen-child/page-templates/page-contact.php
+++ src/wp-content/themes/twentyfifteen-child/page-templates/page-contact.php
@@ -18,12 +18,7 @@
  	<article class="p_content_wordpress">
 		<?php echo wpautop( $post->post_content ); ?>
 	</article>
-
-	<article id="information_map">
-		<h2>Schedule an appointment</h2>
-		<p><a href="mailto:samuel@brikk.se">samuel@brikk.se</a></br>+46 (0)73 612 06 04</p>
-	</article>
-
+	
 	<article id="members" class="clearfix">
 	
 		<?php echo get_contact_faces( $post->ID); ?>
